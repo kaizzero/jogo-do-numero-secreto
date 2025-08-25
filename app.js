1f4dfaf48ff468b53f02ecf00f6f34a9ce774e98
Kaio Rodrigues <kai.rodriguees@gmail.com>
2025-08-25 11:00:53 -0300

    esconder número sorteado do console

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ function gerarNumeroAleatorio(){
         return gerarNumeroAleatorio(); 
     } else {
         listaNumerosSorteados.push(numeroEscolhido);
-        console.log(listaNumerosSorteados);
         return numeroEscolhido;
     }
 }
